f0fb345162d65e1250b2c8c5ba9247030c9319db
JD Jeong <dongmin.jeong@hotmail.com>
2023-05-04 01:44:40 +0900

    [Bronze V] Title: A+B, Time: 228 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] arg) {
+		Scanner sc = new Scanner(System.in);
+		int sc1 = sc.nextInt();
+		int sc2 = sc.nextInt();
+		System.out.println(sc1+sc2);
+	}
+}
