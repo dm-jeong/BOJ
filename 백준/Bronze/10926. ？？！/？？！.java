4b5330fe5b90d6981393693c4028271f6e35b135
JD Jeong <dongmin.jeong@hotmail.com>
2023-05-04 01:50:45 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] arg) {
+		Scanner sc = new Scanner(System.in);
+		String sc2 = sc.next();
+		System.out.println(sc2+"??!");
+	}
+}
