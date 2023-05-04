f71a84045b371ce335c6abbe1bfa9238bec36b02
JD Jeong <dongmin.jeong@hotmail.com>
2023-05-04 11:18:06 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 228 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int sc1 = sc.nextInt();
+    // 불기 - 543 = AD
+        System.out.println(sc1-543);
+    }
+}
