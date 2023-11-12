348e43e26f8cd60951d70b8b4a08d187826f78e8
JEONG Dongmin <dongmin.jeong@hotmail.com>
2023-11-12 20:02:24 +0900

    [Bronze V] Title: 등록, Time: 104 ms, Memory: 13760 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,14 @@
+/* BOJ-7287
+ * 자신이 백준 온라인 저지(BOJ)에서 맞은 문제의 수와 아이디를 그대로 출력하는 프로그램을 작성하시오.
+ * 예제 출력:
+ * 123
+ * Your_ICPC_Team_Name
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		//"맞은 문제"를 출력해야합니다. "맞았습니다"를 출력해서는 안됩니다.
+		System.out.println(25);
+		System.out.println("jd_jeong");
+	}
+}
