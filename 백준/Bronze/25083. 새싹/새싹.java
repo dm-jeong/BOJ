cf0fe2af5c5def98e3c2e700cbf00e6937184db9
JEONG Dongmin <dongmin.jeong@hotmail.com>
2023-11-26 15:51:57 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,22 @@
+/* BOJ-25083 검증수
+ * 문제
+ * 아래 예제와 같이 새싹을 출력하시오.
+ * 
+ * 입력:
+ * 입력은 없다.
+ * 
+ * 출력:
+ * 새싹을 출력한다.
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
