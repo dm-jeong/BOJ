59536f45b568c7bd33ccba993b1f4e7542b48ea6
JEONG Dongmin <dongmin.jeong@hotmail.com>
2025-03-05 00:37:51 +0900

    [Bronze V] Title: A+B, Time: 52 ms, Memory: 5432 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace 백준
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = Console.Read() - 48;
+            int a2 = Console.Read();
+            int a3 = Console.Read() - 48;
+            Console.WriteLine(a1 + a3);
+        }
+    }
+}
