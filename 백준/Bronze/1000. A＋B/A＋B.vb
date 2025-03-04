ce2f4351ecb10670d2b7d51e14c4856113835fab
JEONG Dongmin <dongmin.jeong@hotmail.com>
2025-03-05 00:30:30 +0900

    [Bronze V] Title: A+B, Time: 64 ms, Memory: 5424 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.vb
@@ -0,0 +1,8 @@
+Module Module1
+    Sub Main()
+        Dim m_Text1 As Integer = Console.Read() - 48
+        Dim m_Text2 As Integer = Console.Read()
+        Dim m_Text3 As Integer = Console.Read() - 48
+        Console.WriteLine(m_Text1 + m_Text3)
+    End Sub
+End Module
